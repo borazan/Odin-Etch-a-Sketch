02196d2e830ec0838f6e749d52bb119ee73a1eee
borazan <boratuncel@gmail.com>
2022-03-13 00:09:19 +0100

    Finish main script functionality

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const body = document.querySelector("body");
-const divContainer = document.querySelector(".wrapper");
-divContainer.addEventListener("click", animate);
-
-function animate() {
-    console.log("clicked the wrapper");
+const divContainer = document.createElement("div");
+createGrid(16);
+function createGrid(n) {
+  for (let i = 0; i < n * n; i++) {
+    let tempdiv = document.createElement("div");
+    tempdiv.style.width = "auto";
+    tempdiv.style.height = "auto";
+    tempdiv.style.background = "rgb(255,0,0)";
+    tempdiv.style.opacity = "0";
+    divContainer.appendChild(tempdiv);
+  }
 }
-
-for (let i = 1; i <= 16; i++) {
-  let tempdiv = document.createElement("div");
-  tempdiv.style.width = "auto";
-  tempdiv.style.height = "auto";
-  tempdiv.style.border = "1px dotted grey";
-  divContainer.appendChild(tempdiv);
-}
-
-
-body.appendChild(divContainer);
+divContainer.addEventListener("mouseover", function (e) {
+  let opacity = e.target.style.opacity;
+  let nextOpacity = parseFloat(opacity) + 0.2;
+  console.log(parseFloat(opacity));
+  console.log(parseFloat(nextOpacity));
+  if (parseFloat(opacity) < 0.9) {
+    e.target.style.opacity = `${nextOpacity}`;
+  }
+});
+const pageWrapper = document.querySelector(".pagewrapper");
+divContainer.classList.add("wrapper");
+pageWrapper.appendChild(divContainer);
